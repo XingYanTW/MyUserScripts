d0c865e88b23eff7a3932de49c1955da1b450941
XingYan <46369463+XingYanTW@users.noreply.github.com>
2024-10-26 15:53:56 +0800

    Update YouTube Classic Progress Bar-1.0.user.js

--- YouTube Classic Progress Bar-1.0.user.js
+++ YouTube Classic Progress Bar-1.0.user.js
@@ -4,12 +4,11 @@
 // @version      1.0
 // @description  Revert YouTube's progress bar to a solid color (removes fade to pink at the end).
 // @author       xy
-// @include        *://www.youtube.com/watch?v=*
-// @include        *://www.youtube.com/shorts/*
+// @include      *://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
-// @downloadURL https://github.com/XingYanTW/MyUserScripts/raw/refs/heads/main/YouTube%20Classic%20Progress%20Bar-1.0.user.js
-// @updateURL  https://github.com/XingYanTW/MyUserScripts/raw/refs/heads/main/YouTube%20Classic%20Progress%20Bar-1.0.user.js
+// @downloadURL  https://github.com/XingYanTW/MyUserScripts/raw/refs/heads/main/YouTube%20Classic%20Progress%20Bar-1.0.user.js
+// @updateURL    https://github.com/XingYanTW/MyUserScripts/raw/refs/heads/main/YouTube%20Classic%20Progress%20Bar-1.0.user.js
 // ==/UserScript==
 
 function addStyle() {
@@ -18,7 +17,7 @@ function addStyle() {
         background: #f03 !important;
     }
     ytd-thumbnail-overlay-resume-playback-renderer[enable-refresh-signature-moments-web] #progress.ytd-thumbnail-overlay-resume-playback-renderer {
-	background: #f03 !important;
+	    background: #f03 !important;
     }
     `;
     let style = document.createElement("style");
